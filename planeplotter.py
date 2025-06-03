6e7d2a8c6fac34f60a2c6d1005b3d1992490bd93
asrikanth777 <as140@illinois.edu>
2025-06-02 22:08:57 -0700

    blah

--- planeplotter.py
+++ planeplotter.py
@@ -5,7 +5,10 @@ import pandas as pd
 print(folium.__version__)
 df = pd.read_csv("aircraftSurveillance/aircraft_data.csv")
 
-# sample code to test map thing for folium
-m = folium.Map(location=(45.5236, -122.6750), max_bounds=True)
-m.save("aircraftSurveillance/aircraft_plot.html")
+map = folium.Map(location=(40.1020, -88.2272), max_bounds=True)
+# figure out how to make markers from csv
+
+
+
+map.save("aircraftSurveillance/aircraft_plot.html")
 
