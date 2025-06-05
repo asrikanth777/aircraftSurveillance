82673c494ef3d3735c1f129980f81d7924c3bb3d
asrikanth777 <as140@illinois.edu>
2025-06-04 20:02:53 -0700

    populated map with military aircraft currently flying, not many are showing from other countries, just US for the most part tho....

--- planeplotter.py
+++ planeplotter.py
@@ -1,14 +1,46 @@
 import folium
 import pandas as pd
+import random
 
-
-print(folium.__version__)
 df = pd.read_csv("aircraftSurveillance/aircraft_data.csv")
+
+color_list = ['red', 'blue', 'green', 'purple', 'orange', 'darkred', 'lightred',
+ 'beige', 'darkblue', 'darkgreen', 'cadetblue', 'darkpurple', 
+ 'white', 'pink', 'lightblue', 'lightgreen', 'gray', 'black', 'lightgray']
+
 
 map = folium.Map(location=(40.1020, -88.2272), max_bounds=True)
 # figure out how to make markers from csv
+for index, row in df.iterrows():
+    if row['lat'] != 0 and row['lon'] != 0:
+        html=f"""
+            <b>Callsign:</b> {row['r']} <br>
+            <b>Type:</b> {row['desc']}<br>
+            <b>Hex:</b> {row['hex']}<br>
+            <b>Altitude:</b> {row['alt_baro']} ft<br>
+            <b>Speed:</b> {row['gs']} kt<br>
+            <b>Heading:</b> {row['track']}°
+        """
+        iframe = folium.IFrame(html=html, width=250, height=150)
+        popup = folium.Popup(iframe, max_width=300)
+        col = random.choice(color_list)
+        folium.Marker(
+            location=[row['lat'], row['lon']],
+            popup=popup,
+            icon=folium.Icon(color=col, icon="arrow-up", prefix='fa', angle=row['track'])
+        ).add_to(map)
 
 
 
 map.save("aircraftSurveillance/aircraft_plot.html")
 
+
+
+"""
+folium.Marker(
+    location=[40.1020, -88.2272],
+    popup="UIUC C-130\nAltitude: 18000 ft",
+    tooltip="Click for details",
+    icon=folium.Icon(color="blue", icon="plane", prefix='fa')
+)
+"""
